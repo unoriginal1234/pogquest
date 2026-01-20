1aace708b0b67efe01c137e61c91d04ff7b37365
unoriginal1234 <eric.t.roth@gmail.com>
2026-01-20 08:50:14 -0800

    Fixed bug with closing chapters

--- pog-quest/src/classes/Story.ts
+++ pog-quest/src/classes/Story.ts
@@ -14,30 +14,40 @@ export default class Story {
         this.description = description;
         this.chapters = chapters;
         this.unlockedChapters = [0, 1];
-        this.currentChapter = this.chapters[0];
-        this.chapterNumber = 1;
+        this.chapterNumber = 0;
+        this.currentChapter = this.chapters[this.chapterNumber];
         this.nextUnlockedChapter = Math.max(...this.unlockedChapters) + 1;
     }
 
     // story should handle the chapter progression and completion
 
+    // takes in a chapter index and sets the current chapter
     setCurrentChapter(chapterNumber: number) {
         this.currentChapter = this.chapters[chapterNumber];
         this.chapterNumber = chapterNumber;
     }
 
+    // closes the current chapter and unlocks the next chapter
+    // removes it from the unlocked chapters
+    // if there is a next chapter, sets the next unlocked chapter to the next chapter
+    // sets the current chapter to the first unlocked chapter
+    // sets the chapter number to the first unlocked chapter
     closeChapter(){
-        this.unlockedChapters.filter(chapter => chapter === this.chapterNumber);
+        this.removeUnlockedChapter(this.chapterNumber);
         if (this.nextUnlockedChapter < this.chapters.length) {
+            this.setUnlockedChapter(this.nextUnlockedChapter);
             this.nextUnlockedChapter++;
         }
-        this.unlockedChapters.push(this.nextUnlockedChapter);
-        this.currentChapter = this.chapters[this.unlockedChapters[0]];
         this.chapterNumber = this.unlockedChapters[0];
+        this.currentChapter = this.chapters[this.chapterNumber];
     }
 
     getChapterNumber() {
-        return this.chapterNumber + 1;
+        return this.chapterNumber;
+    }
+
+    removeUnlockedChapter(chapterNumber: number) {
+        this.unlockedChapters = this.unlockedChapters.filter(chapter => chapter != chapterNumber);
     }
 
     getCurrentChapter() {
--- pog-quest/src/components/GameStoryPanel.tsx
+++ pog-quest/src/components/GameStoryPanel.tsx
@@ -5,13 +5,16 @@ import Chapter from "../classes/Chapter";
 
 export default function GameStoryPanel({ game }: { game: Game }) {
 
-    const [story] = useState<Story>(game.getStory());
+    const [story, setStory ] = useState<Story>(game.getStory());
+    console.log("Story", story);
     const [currentChapter, setCurrentChapter] = useState<Chapter>(story.getCurrentChapter());
     const [chapterNumber, setChapterNumber] = useState<number>(story.getChapterNumber());
     const [completionType, setCompletionType] = useState(currentChapter.getCompletionType());
     const [unlockedChapters, setUnlockedChapters] = useState<number[]>(story.getUnlockedChapters());
     // const [unlockedChapterCount, setUnlockedChapterCount] = useState<number>(unlockedChapters.length);
     // const [totalChapterCount] = useState<number>(story.getChapterCount());
+
+    console.log("Unlocked chapters in component", unlockedChapters);
 
     function handleChapterClick(chapterNumber: number) {
         console.log("Setting chapter number to", chapterNumber);
@@ -20,38 +23,47 @@ export default function GameStoryPanel({ game }: { game: Game }) {
         setCurrentChapter(nextChapter);
         setChapterNumber(story.getChapterNumber());
         setCompletionType(nextChapter.getCompletionType());
-        
-        // setUnlockedChapterCount(nextUnlockedChapters.length);
+        setStory(story);
     }
 
     function handleCloseCurrentChapter() {
+        console.log("Unlocked chapters before closing", story.getUnlockedChapters());
         story.closeChapter();
+        console.log("Unlocked chapters after closing", story.getUnlockedChapters());
+
         setUnlockedChapters(story.getUnlockedChapters());
         setChapterNumber(story.getChapterNumber());
         setCurrentChapter(story.getCurrentChapter());
         setCompletionType(currentChapter.getCompletionType());
+        setStory(story);
     }
 
+    // need to handle the case where the current chapter is the last chapter
+    // for now, we'll just disable the button if the current chapter is the last chapter
    
     return (
         <section className="demo-section pog-border">
             <h2>{story.getTitle()}</h2>
-            <p className="pog-glow-blue">
-                Chapter {chapterNumber}:
+            <span className="pog-glow-blue">
+                Chapter {chapterNumber} :
                 <p className="pog-glow-green">{currentChapter.getTitle()}</p>
                 <p>{currentChapter.getDescription()}</p>
-            </p>
+            </span>
            
             <p className="pog-glow-blue">Completion Type: {completionType.constructor.name}</p>
 
             <div className="button-group">
                 {unlockedChapters.map((chapterNumber: number) => (
                     <button key={chapterNumber} onClick={() => handleChapterClick(chapterNumber)}>
-                        {chapterNumber  + 1}
+                        {chapterNumber}
                     </button>
                 ))}
             </div>
-            <button onClick={handleCloseCurrentChapter}>Close Current Chapter</button>
+            <button 
+                onClick={handleCloseCurrentChapter}
+                disabled={chapterNumber === story.getChapterCount() - 1}>
+                    Close Current Chapter
+            </button>
         </section>
     );
 }
