60a746b73872ed7f66d93875cd09f3833fdca6d8
unoriginal1234 <eric.t.roth@gmail.com>
2026-02-28 18:29:46 -0800

    Stack has placeholder when out of pogs and no longer accepts click event

--- pog-quest/src/components/StackComponent.tsx
+++ pog-quest/src/components/StackComponent.tsx
@@ -6,16 +6,20 @@ export default function StackComponent({ stack, onClick }: { stack: PogClass[], 
     
 
     return (
-        <div className="match-stack" onClick={onClick} >
-            {stack.map((pog, index) => (
-                <div
-                    key={pog.getId()}
-                    className="match-stack-item"
-                    style={{ zIndex: stack.length - index }}
-                >
-                    <Pog pog={pog} isFlippedUp={true} />
-                </div>
-            ))}
+        <div className="match-stack" onClick={stack.length > 0 ? onClick : undefined} >
+            {stack.length === 0 ? (
+                <div className="empty-stack-placeholder" />
+            ) : (
+                stack.map((pog, index) => (
+                    <div
+                        key={pog.getId()}
+                        className="match-stack-item"
+                        style={{ zIndex: stack.length - index }}
+                    >
+                        <Pog pog={pog} isFlippedUp={true} />
+                    </div>
+                ))
+            )}
         </div>
     );
 }
